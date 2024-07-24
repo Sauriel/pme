256d5ce0122470836bab25876e8a16e56583c1f5
Sascha Münchow <smuenchow@novomind.com>
2024-07-24 07:45:42 +0200

    added BleedingTile

--- types/tile/type.d.ts
+++ types/tile/type.d.ts
@@ -9,6 +9,24 @@ export type MapTile = {
   continueBottomLeft?: boolean;
 }
 
+export type Tile = {
+  x: number;
+  y: number;
+
+}
+
+export type BleedingTile = {
+  center: Tile;
+  topLeft: Tile;
+  top: Tile;
+  topRight: Tile;
+  right: Tile;
+  bottomRight: Tile;
+  bottom: Tile;
+  bottomLeft: Tile;
+  left: Tile;
+}
+
 export interface BaseTilemap {
   draw(context: CanvasRenderingContext2D, tile: MapTile, x: number, y: number, frame: number);
 }
